1b3110dac35868b1c4e27b9e3d221b98a6c4a7c0
Ricieri Vasconcellos <ricieribressiani07@gmail.com>
2026-06-22 00:05:54 -0300

    refactor: pagarComXP não utiliza mais setXP() pois foi excluido e agora utiliza removerXP(), e foi adicionado a exception em caso de erro direto no método.

--- src/br/edu/cafeteria/modelo/ClienteVIP.java
+++ src/br/edu/cafeteria/modelo/ClienteVIP.java
@@ -2,36 +2,33 @@ package br.edu.cafeteria.modelo;
 
 import br.edu.cafeteria.excecao.PontosInsuficientesException;
 
-
 public class ClienteVIP extends Cliente {
 
-    public static final int TAXA_CONVERSAO = 10; // 10 XP = R$ 1;
+    private static final int TAXA_CONVERSAO = 10; // 10 XP = R$ 1;
 
-    //Construtor;
-    public ClienteVIP(String nome, String cpf)
-    {
+    // Construtor;
+    public ClienteVIP(String nome, String cpf) {
         super(nome, cpf);
     }
 
-    
-    //Sobrescrita do metodo abstrato para o VIP;
-    public int calcularPontos(double valorGasto)
-    {
-        return (int) (valorGasto*2); //2Xp por real gasto;
+    // Sobrescrita do metodo abstrato para o VIP;
+    public int calcularPontos(double valorGasto) {
+        return (int) (valorGasto * 2); // 2Xp por real gasto;
     }
 
-    //Método exclusivo para pagar com XP;
-    public void pagarComXP(double valorTotal) throws PontosInsuficientesException
-    {
-        int xpNecessario = (int) (valorTotal *TAXA_CONVERSAO);
-        if(getSaldoXP() < xpNecessario)
-        {
-            throw new PontosInsuficientesException("Saldo de XP insuficiente. Necessário: " + xpNecessario + ", atual: " + getSaldoXP());
-        } else {
-            setSaldoXP(getSaldoXP() - xpNecessario);
+    // Método exclusivo para pagar com XP;
+    public void pagarComXP(double valorTotal) throws PontosInsuficientesException {
+        int xpNecessario = (int) (valorTotal * TAXA_CONVERSAO);
+        try {
+            if (getSaldoXP() < xpNecessario) {
+                throw new PontosInsuficientesException(
+                        "Saldo de XP insuficiente. Necessário: " + xpNecessario + ", atual: " + getSaldoXP());
+            } else {
+                removerXP(xpNecessario);
+            }
+        } catch (PontosInsuficientesException e) {
+            System.out.println(e.getMessage());
         }
+
     }
-
-
-
 }
