ad6d388c384abe03bde94afdafb566bb7bd2f298
Ricieri Vasconcellos <ricieribressiani07@gmail.com>
2026-06-21 23:49:08 -0300

    refactor: remove código desnecessário

--- src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
+++ src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
@@ -1,10 +1,6 @@
 package br.edu.cafeteria.excecao;
 
 public class EstoqueInsuficienteException extends Exception {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	public EstoqueInsuficienteException(String mensagem) {
         super(mensagem);
